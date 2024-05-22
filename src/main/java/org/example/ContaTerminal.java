706f6403d1adbd9700df25df23ae87f8d2beb7c7
Taylormeira <taylortmeira25@gmail.com>
2024-05-22 19:32:49 -0300

    Ajustes em looping

--- src/main/java/org/example/ContaTerminal.java
+++ src/main/java/org/example/ContaTerminal.java
@@ -1,6 +1,5 @@
 package org.example;
 
-import java.sql.SQLOutput;
 import java.util.Scanner;
 
 import static java.lang.System.exit;
@@ -9,9 +8,9 @@ public class ContaTerminal {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int numero = 0, selecao = 0 ;
-        String opcoes, Agencia, nomeCliente;
-        Float saldo = 0.0F;
+        int numero = 0, selecao = 0;
+        String opcoes, agencia, nomeCliente;
+        Float saldo = 0.0F, deposito = 0.0F, saque = 0.0F;
 
         opcoes = "Selecione a opção que deseja \n" +
                 "1| Criar conta Nova \n" +
@@ -21,26 +20,40 @@ public class ContaTerminal {
         System.out.println(opcoes);
         selecao = scanner.nextInt();
 
-        while (selecao < 3) {
+        while (selecao <= 3) {
             if (selecao == 1) {
                 criarConta();
             } else if (selecao == 2) {
+                System.out.println("informe o numero da agenda e da conta");
+                agencia = scanner.next();
+                numero = scanner.nextInt();
+                System.out.println("Informe o valor que deseja depositar");
+                deposito = scanner.nextFloat();
+                if (deposito <= 0) {
+                    System.out.println("Não é possível ter saldo negativo");
 
-            } else {
+                } else {
+                    saldo = saldo + deposito;
+                }
 
+            } else if (selecao == 3) {
+                System.out.println("Infome o valor que deseja retirar: ");
+                saque = scanner.nextFloat();
+                if (saque <= 0) {
+                    System.out.println("Não se pode sacar um valor negativo ou zero");
+                } else if (saque > saldo) {
+                    System.out.println("O valor solicitado é maior que o valor que tem de saldo");
+                } else {
+                    saldo = saldo - saque;
+                    System.out.println("Retirado o valor de " + saque + " de sua conta restou o saldo de " + saldo );
+                }
             }
-
             System.out.println(opcoes);
             selecao = scanner.nextInt();
         }
-        if (saldo <= 0) {
-            System.out.println("Não é possível ter saldo negativo");
-            exit(0);
-        }
-
     }
 
-    private static void criarConta(){
+    private static void criarConta() {
         int numero;
         String agencia, nomeCliente;
         Float saldo;
